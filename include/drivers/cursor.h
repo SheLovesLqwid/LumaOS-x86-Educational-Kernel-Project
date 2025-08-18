9fc9506e617ec9140c929374bf08724ad07ab4df
Kratos v2 bc im him <jryan75707@icloud.com>
2025-08-18 17:45:17 -0500

    Create cursor.h

---
+++ include/drivers/cursor.h
@@ -0,0 +1,11 @@
+#ifndef LUMAOS_CURSOR_H_
+#define LUMAOS_CURSOR_H_
+
+#include <stdint.h>
+
+// function declarations
+void draw_cursor(int32_t x, int32_t y);
+void clear_cursor(int32_t x, int32_t y);
+void update_cursor(int32_t x, int32_t y);
+
+#endif // LUMAOS_CURSOR_H_
