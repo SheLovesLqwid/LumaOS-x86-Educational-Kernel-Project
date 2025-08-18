abb387066cdc66eba478825c8c829cca1bcfc9e3
Kratos v2 bc im him <jryan75707@icloud.com>
2025-08-18 17:23:11 -0500

    Update init.c

--- apps/init/init.c
+++ apps/init/init.c
@@ -1,10 +1,25 @@
-#include "../../include/libc/stdio.h"
+#include <libc/stdio.h>
+#include <fs/filesystem.h>
+
+// forward declaration of our test function
+extern void fs_test();
 
 int main(int argc, char **argv)
 {
-    printf("Hello World!");
-
-    while(1) __asm__ volatile("nop");
+    printf("LumaOS Initialization Process\n");
+    printf("============================\n\n");
+    
+    // run filesystem test
+    printf("Running filesystem test...\n");
+    fs_test();
+    
+    printf("\nInitialization complete. Starting shell...\n\n");
+    
+    // start the shell or main system process
+    while(1) {
+        // this is where the shell would run
+        __asm__ volatile("hlt");
+    }
 
     return 0;
 }
